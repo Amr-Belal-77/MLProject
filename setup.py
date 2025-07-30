ea51da3bfce9101fead930c361007bede12233ad
Amr-Belal-77 <amrjmika204@gmail.com>
2025-07-30 16:51:50 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path)-> List[str]:
+    """
+    This function will return the list of requirements.
+    """
+    requirements = []
+    with open(file_path, 'r') as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", " ") for req in requirements ]
+
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Amr Belal',
+    author_email='amrjmika204@gmail.com',
+    description='A machine learning project template',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
